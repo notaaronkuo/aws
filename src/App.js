31e641d570e28f8f6d2c7f3111d063eb5cbdc0ef
Aaron Kuo <kuoaaron4398@gmail.com>
2023-02-26 11:27:09 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. So I've made some changes, look into react components for html
         </p>
         <a
           className="App-link"
